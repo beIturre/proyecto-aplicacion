241e57fd18288794113fae6e5aacde3d4f8ef589
beIturre <be.iturre@duocuc.cl>
2024-09-05 19:57:19 -0400

    Cambios para visualizar app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-
+import { addIcons } from 'ionicons';
+import { logoIonic } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+
+    addIcons({ logoIonic });
+
+
+  }
 }
 
